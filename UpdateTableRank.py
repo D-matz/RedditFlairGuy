2d9c1c27eb09f107f1b10e32927c140fb30c2872
D-matz <davidedmatz@gmail.com>
2023-05-20 08:28:36 -0500

    use region, not just NA

--- UpdateTableRank.py
+++ UpdateTableRank.py
@@ -3,8 +3,8 @@ import requests
 
 riot_api_key = "RGAPI-40d9ab03-b4ff-4656-be46-775d45b2eaed"
 
-def riot_wrapper(req):
-    url = req + "?api_key=" + riot_api_key
+def riot_wrapper(req, region):
+    url = "https://" + region + ".api.riotgames.com/" + req + "?api_key=" + riot_api_key
     print(url)
     resp = requests.get(url)
     if resp.status_code == 200:
@@ -19,13 +19,15 @@ entity = table_client.get_entity(partition_key="pkey", row_key="rkey")
 entities = table_client.query_entities(query_filter="")
 
 for entity in entities:
-    summonerid = entity.get("SummonerID")
-    print(summonerid)
-    league_info = riot_wrapper("https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + summonerid)
+    summonerid = entity["SummonerID"]
+    region = entity["Region"]
+    print(summonerid, region)
+    league_info = riot_wrapper("lol/league/v4/entries/by-summoner/" + summonerid, region)
     for queue_info in league_info:
         if queue_info['queueType'] == "RANKED_SOLO_5x5":
             rank = queue_info['rank']
             tier = queue_info['tier']
             entity["Rank"] = rank
             entity["Tier"] = tier
+            print(tier, rank)
             table_client.update_entity(entity, mode=UpdateMode.MERGE)
