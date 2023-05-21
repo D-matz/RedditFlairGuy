becf9f554a3ce9c67ff8899b0a4b9b38b69576be
D-matz <davidedmatz@gmail.com>
2023-05-20 22:58:53 -0500

    put secrets in DependencyList.py

--- UpdateTableRank.py
+++ UpdateTableRank.py
@@ -1,7 +1,7 @@
 from azure.data.tables import TableClient, UpdateMode
 import requests
 
-riot_api_key = "RGAPI-40d9ab03-b4ff-4656-be46-775d45b2eaed"
+from DependencyList import riot_api_key, connection_string
 
 def riot_wrapper(req, region):
     url = "https://" + region + ".api.riotgames.com/" + req + "?api_key=" + riot_api_key
@@ -12,7 +12,6 @@ def riot_wrapper(req, region):
     else:
         return None
 
-connection_string = "DefaultEndpointsProtocol=https;AccountName=flair20;AccountKey=xZcfxarFvfFV9JvZmFB6gx7r6W4pSo0CFtLluhuvZLcNhI4IbEJb3uFConvLirKCjfvKjS6N6JSy+AStgn0P0w==;EndpointSuffix=core.windows.net"
 table_client = TableClient.from_connection_string(connection_string, table_name="SchoolSummoner")
 
 entity = table_client.get_entity(partition_key="pkey", row_key="rkey")
@@ -23,11 +22,17 @@ for entity in entities:
     region = entity["Region"]
     print(summonerid, region)
     league_info = riot_wrapper("lol/league/v4/entries/by-summoner/" + summonerid, region)
-    for queue_info in league_info:
-        if queue_info['queueType'] == "RANKED_SOLO_5x5":
-            rank = queue_info['rank']
-            tier = queue_info['tier']
-            entity["Rank"] = rank
-            entity["Tier"] = tier
-            print(tier, rank)
-            table_client.update_entity(entity, mode=UpdateMode.MERGE)
+    unranked = True
+    if league_info != None:
+        for queue_info in league_info:
+            if queue_info['queueType'] == "RANKED_SOLO_5x5":
+                rank = queue_info['rank']
+                tier = queue_info['tier']
+                entity["Rank"] = rank
+                entity["Tier"] = tier
+                unranked = False
+    if unranked:
+        entity["Rank"] = "unranked"
+        entity["Tier"] = ""
+    print(entity["Rank"], entity["Tier"])
+    table_client.update_entity(entity, mode=UpdateMode.MERGE)
